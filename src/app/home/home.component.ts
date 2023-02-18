223589f11682e367a3f83f1e990ad72ac57a15d8
GhanmiMelek <ghanmimelek72@gmail.com>
2023-02-18 20:09:53 +0100

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+  email: string | undefined;
+  password: string | undefined;
+
+
+onLoginSubmit() {
+  // TODO: Implement login functionality
+}
+}
+
+ 
